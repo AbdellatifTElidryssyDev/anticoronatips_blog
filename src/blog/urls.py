492cc6785acddcc66ebcf02cca755afe9b82bfc5
khalid hassan <buyukpatron@gmail.com>
2020-03-20 20:10:27 +0100

    create parts of website and do some styles

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name='home'),
+    path('hadiths/',views.hadiths, name='hadiths'),
+    path('videos/',views.videos, name='videos'),
+    path('coronaintheworld/',views.coronaintheworld, name='coronaintheworld'),
+    path('statistics/', views.statistics, name='statistics'),
+
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,4 +2,25 @@ from django.shortcuts import render
 
 def home(request):
 
-    return render(request, 'blog/index.html', {'title':'home'})
+    context = {
+
+            'title':'الصفحة الرئيسية',
+            'content-section':'contents',
+        }
+
+    return render(request, 'blog/index.html', context)
+
+
+def hadiths(request):
+    return render(request, 'blog/hadiths.html')
+
+
+def videos(request):
+     return render(request, 'blog/videos')
+
+def coronaintheworld(request):
+    return render(request, 'blog/coronaintheworld')
+
+def statistics(request):
+    return render(request, 'blog/statistics')
+        
